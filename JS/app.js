cf47019fbad6f245f29db36e1e1a1b643e498e61
mabirhire1 <mabirhire1@gmail.com>
2023-12-20 01:32:00 +0100

    modified html file

--- JS/app.js
+++ JS/app.js
@@ -1,21 +1,50 @@
 const swiper = new Swiper('.swiper', {
     // Optional parameters
-    direction: 'vertical',
+    speed: 1000,
+    direction: 'horizontal',
+
+    //keyboard
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+
+    //mousewheel
+    mousewheel: {
+      invert: true,
+    },
+
     loop: true,
+    //autp play
+   autoplay: {
+     delay: 2000,
+   },
   
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
+   
+    //  breakpoints
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+      },
   
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+      //when the window width is >=1024px
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
   
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
+      //when the window width is >=2560px
+      2560: {
+        slidesPerView: 4,
+        spaceBetween: 30,
+      },
+    }, 
   });
